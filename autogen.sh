781d19cf2663b83740dcd0e7b9c60c64202d0f2d
Nate Coraor <nate@bx.psu.edu>
2018-11-08 21:12:28 -0500

    Autotools updates:
    - Set 'foreign' in configure.ac so that it doesn't have to be passed on
      the command line. Especially useful for Debian's dh-autoreconf (and
      the Debian package was patching this already anyway).
    
    - Use autoreconf instead of calling individual autotools. Again, Debian
      has already been building it this way for years. autogen.sh is still
      there if you want to call it - it does some dependency checks that
      provide a nicer user experience than autoreconf's failure message when
      a dependency is missing.

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-# $Id$
 
 run() {
 	echo "running $@ ($PWD)"
@@ -22,27 +21,13 @@ AUTOHEADER=${AUTOHEADER:=autoheader}
 AUTOCONF=${AUTOCONF:=autoconf}
 LIBTOOLIZE=${LIBTOOLIZE:=libtoolize}
 AUTOMAKE=${AUTOMAKE:=automake}
+AUTORECONF=${AUTORECONF:=autoreconf}
 
 check $ACLOCAL
 check $AUTOHEADER
 check $AUTOCONF
 check $LIBTOOLIZE
 check $AUTOMAKE
+check $AUTORECONF
 
-mkdir -p scripts
-
-run ${ACLOCAL} -I m4 | grep -v ^/usr/share/aclocal || exit 1
-run ${LIBTOOLIZE} --automake --copy --force || exit 1
-run ${AUTOHEADER} --warnings=all || exit 1
-run ${AUTOMAKE} --foreign --add-missing --copy --warnings=all || exit 1
-run ${AUTOCONF} --warnings=all -Wno-obsolete || exit 1
-
-if [ -n "$*" ]; then
-	args="$*"
-elif [ -f config.log ]; then
-	args=`grep '\$ *\./configure ' config.log \
-			 | sed 's:^ *\$ *\./configure ::;s:--no-create::;s:--no-recursion::' \
-			 2>/dev/null`
-fi
-
-run ./configure ${args}
+run autoreconf --install --force
